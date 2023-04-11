a5d02659abae837c33926412236124b5e0ca8c04
Ascetholix <sangak@mail.ru>
2023-04-11 10:18:10 +0300

    add win

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import toys.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -11,19 +12,28 @@ public class Main {
     static ArrayList arrayList = new ArrayList<>();
     public static void main(String[] args) throws IOException {
 
-        Scanner iScanner = new Scanner(System.in);
+        Random rnd = new Random();
+        Scanner scanner = new Scanner(System.in);
 
-//        System.out.printf("Привет, %s!", name);
-//        iScanner.close();
-//        FileWriter fileWriter = new FileWriter("toys_log.txt");
-//        fileWriter.flush();
         arrayList.addAll(CreateToys());
-//        int count = 3;
-        while (true){
-            String name = iScanner.nextLine();
+        String choiceToys = ChoiceToys();
+        int count = 0;
+        while (count != 2){
+
+            System.out.println("Список игрушок:");
             System.out.println(arrayList);
-            System.out.println(arrayList.size());
-            System.out.println(getCount());
+            int winningNumber = rnd.nextInt(1,getCount());
+            System.out.printf("Выпало игрушка под номером %d\n",winningNumber);
+            if (Winning(arrayList,choiceToys,winningNumber)){
+                arrayList.remove(winningNumber);
+                System.out.println("Осталось игрушок");
+                System.out.println(arrayList);
+                break;
+            }
+            System.out.println("Повторите");
+            System.out.printf("У вас %d хода\n", 2-count);
+            scanner.nextLine();
+            count++;
         }
 
     }
@@ -64,4 +74,32 @@ public class Main {
         return arrayList;
     }
 
+    public static String ChoiceToys(){
+        Scanner iScanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Выберите пожалуйста призовую игрушку:");
+            System.out.println("1 - Солдатик\n" +
+                    "2 - Медвежонок\n" +
+                    "3 - Барби\n");
+            int numberOfToys = Integer.parseInt(iScanner.nextLine());
+            switch (numberOfToys) {
+                case 1:
+                    return "Солдатик";
+                case 2:
+                    return "Медвежонок";
+                case 3:
+                    return "Барби";
+            }
+        }
+    }
+    public static boolean Winning(ArrayList<Toys> arrayList, String choice, int number){
+        if(arrayList.get(number).toString().equals(choice)) {
+            System.out.printf("Ура!!! Вы виграли: %s\n", choice);
+            return true;
+        }
+        else {
+            System.out.println("Вы проиграли");
+            return false;
+        }
+    }
 }
